4d24514eecef0776858db0e01c405129ce9b726c
Sebastian Tillmann <sebastian.tillmann@condor.com>
2021-02-14 12:13:22 +0100

    minor edits

--- kaliases.sh
+++ kaliases.sh
@@ -2,8 +2,11 @@
 
 # Download kubectl aliases and put them in .bashrc
 
+echo "Download kubectl aliases"
+
 wget https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases -O .kubectl_aliases
 
+echo "put aliases in .bashrc"
 
 echo '[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases' >> ~/.bashrc
 
@@ -11,6 +14,10 @@ source ~/.bashrc
 
 # Get my .vimrc
 
+echo "fetch my .vimrc"
+
 wget https://raw.githubusercontent.com/dtadrk/cka/main/myvimrc -O myvimrc
+
+echo "enable my .vimrc"
 
 cat myvimrc > ~/.vimrc
