f6f3bd846bc08bb2f4cf49154c59a22c91ce2b73
Sebastian Tillmann <sebastian.tillmann@condor.com>
2021-02-14 11:58:45 +0100

    script modified to get vimrc

--- kaliases.sh
+++ kaliases.sh
@@ -1,4 +1,6 @@
-git remote add origin git@github.com:dtadrk/#!/usr/bin/env bash
+#!/usr/bin/env bash
+
+# Download kubectl aliases and put them in .bashrc
 
 wget https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases -O .kubectl_aliases
 
@@ -6,3 +8,9 @@ wget https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_al
 echo [ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases >> ~/.bashrc
 
 source ~/.bashrc
+
+# Get my .vimrc
+
+wget https://raw.githubusercontent.com/dtadrk/cka/main/myvimrc -O myvimrc
+
+cat myvimrc > ~/.vimrc
