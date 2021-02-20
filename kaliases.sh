cf35ddc1e6f52c9606daf432be2f96283aae454f
Sebastian Tillmann <sebastian.tillmann@condor.com>
2021-02-20 11:06:35 +0100

    tweaked vimrc to delete whitespace

--- kaliases.sh
+++ kaliases.sh
@@ -27,3 +27,4 @@ cat myvimrc > ~/.vimrc
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
